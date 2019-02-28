16628e99b8cde5ec0b0b721419fccba0a4d633d8
a1532422135 <37723264+a1532422135@users.noreply.github.com>
2019-02-28 15:16:10 +0800

    eureka服务注册中心

--- springcloud/cloudcenter/src/main/java/com/springcloud/center/SpringCloudConsumerApplication.java
+++ springcloud/cloudcenter/src/main/java/com/springcloud/center/SpringCloudConsumerApplication.java
@@ -1,4 +1,4 @@
-package com.springcloud.consumer;
+package com.springcloud.center;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
